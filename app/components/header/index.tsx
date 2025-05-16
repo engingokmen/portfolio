6e5e896a1fe93d4ce6c4883853a6a6fbe006ff85
Engin Gokmen <engin.gokmen@icloud.com>
2025-05-16 13:24:01 +0300

    updates header

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { settings } from "@/app/settings";
 import styles from "./styles.module.css";
+import styleLink from "../linkNav/styles.module.css";
 import { pathName } from "@/app/utilities";
 import { LinkNav } from "../linkNav";
 import Image from "next/image";
@@ -10,11 +11,23 @@ import Link from "next/link";
 const { title, githubHomepageLink } = settings;
 
 export const Header = () => {
-  const links = settings.projectList.map((project) => (
-    <LinkNav key={project.title} href={`/${pathName(project.title)}`}>
-      {project.title}
-    </LinkNav>
-  ));
+  const links = settings.projectList.map((project) => {
+    const href = project.link ? project.link : pathName(project.title);
+
+    if (project.link) {
+      return (
+        <Link target="_blank" href={`${href}`} className={styleLink.link}>
+          {project.title}
+        </Link>
+      );
+    }
+
+    return (
+      <LinkNav key={project.title} href={`/${href}`}>
+        {project.title}
+      </LinkNav>
+    );
+  });
 
   return (
     <>
